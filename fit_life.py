20268a9c947b064cdea1326194ad4bcda66aee70
egordonskoj9-sys <egordonskoj9@gmail.com>
2026-08-30 01:54:27 +0500

    Add files via upload

--- fit_life.py
+++ fit_life.py
@@ -6,13 +6,11 @@ print(f"Отлично! Тебе {user_age} лет. Скоро будем счи
 user_weight = float(input("Твой вес в кг (например: 67.67): "))
 user_height = float(input("Твой рост в метрах (например: 1.67): "))
 bmi = user_weight / (user_height ** 2)
-bmi_rounded = round(bmi,1)
-water_l = (user_weight * 30)/1000
+bmi_rounded = round(bmi, 1)
+water_l = (user_weight * 30) / 1000
 print("\n--- Твой фитнес-отчёт ---")
 print(f"Пользователь: {user_name} ({user_age} лет)")
 print(f"Индекс массы тела (ИМТ): {bmi_rounded}")
 print(f"Рекомендуемая норма воды: {water_l:.2f} л в день")
 print("--------------------------")
 print("Вот твой отчет. Молодец, что интересуешься и думаешь о здоровье!")
-
-
