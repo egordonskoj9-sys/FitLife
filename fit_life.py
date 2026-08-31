3e107a83bc3cc3e0fee68811a2b73fecb95e46b2
egordonskoj9-sys <egordonskoj9@gmail.com>
2026-08-31 14:55:49 +0500

    Add files via upload

--- fit_life.py
+++ fit_life.py
@@ -1,16 +1,16 @@
 ML_PER_KG = 30       # Норма воды: 30 мл на 1 кг веса
-ML_IN_LITER = 1000   #   В одном литре 1000 миллилитр
+ML_IN_LITER = 1000   # В одном литре 1000 миллилитр
 
 print("Привет, я твой фитнес-бот!")
 user_name = input("Как тебя зовут? ").strip()
 print(f"Приятно познакомиться, {user_name}!")
 while True:
     try:
-         age_input = input("Введите ваш возраст (только число): ")
-         age = int(age_input)
-         break
+        age_input = input("Введите ваш возраст (только число): ")
+        age = int(age_input)
+        break
     except ValueError:
-        print("Ошибка: возраст должен быть числом! Например: 25. Попробуйте ещё раз.")
+        print("Ошибка: возраст должен быть числом!" "Например: 25. Попробуйте ещё раз.")
 while True:
     try:
         weight_input = input("Введите ваш вес в кг (например, 67.67): ")
@@ -24,7 +24,7 @@ while True:
         user_height = float(height_input)
         break
     except ValueError:
-        print("Ошибка: рост должен быть числом! Попробуйте ещё раз.") 
+        print("Ошибка: рост должен быть числом! Попробуйте ещё раз.")
 bmi = user_weight / (user_height ** 2)
 bmi_rounded = round(bmi, 1)
 water_l = (user_weight * 30) / 1000
